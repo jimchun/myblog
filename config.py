7e661b403496d7c9111346140c77b9cd91e9f028
jimchun <32630158+jimchun@users.noreply.github.com>
2024-09-09 10:46:19 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:Zoe%409820@localhost/flask_blog'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:yourpassword@localhost/flask_blog' #替换你的密码yourpassword
     SQLALCHEMY_TRACK_MODIFICATIONS = False
